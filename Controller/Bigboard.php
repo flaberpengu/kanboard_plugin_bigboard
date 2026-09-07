ee517ff0872c873084d70a262c031c66afbfd3c1
flaberpengu <53153483+flaberpengu@users.noreply.github.com>
2026-09-07 23:23:11 +0100

    Change default BigBoard button to filter open issues only

--- Controller/Bigboard.php
+++ Controller/Bigboard.php
@@ -55,7 +55,7 @@ class Bigboard extends BaseController
     {
         $user = $this->getUser();
         $project_ids = $this->bigboardModel->selectFindAllProjectsById($user['id']);
-        $search = urldecode($this->request->getStringParam('search', $this->userSession->getBigboardSearch()) ?? '');
+        $search = urldecode($this->request->getStringParam('search', $this->userSession->getBigboardSearch() ?? 'status:open'));
         $this->userSession->setBigboardSearch($search);
         $nb_projects = count($project_ids);
 
